98aead5cd03c04833b389ae8eefd286c1602bbac
Rostislav <93567572+if1t@users.noreply.github.com>
2022-02-16 20:35:32 +0800

    Update min_max.cpp

--- min_max.cpp
+++ min_max.cpp
@@ -1,35 +1,58 @@
 #include <iostream>
 #include <omp.h>
+#include <random>
 
 using namespace std;
 
-int main()
-{
+int main() {
+    setlocale(0, "");
+    random_device rd;
+    mt19937 mersenne(rd()); // инициализируем Вихрь Мерсенна случайным стартовым числом 
+
     int n;
+    double st; // переменная для установки отсчета времени
+    double end; // для фиксирования конца работы алгоритма 
     cin >> n;
+
     int* a = new int[n];
     int max = INT16_MIN, min = INT16_MAX;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+
+    for (long i = 0; i < n; i++) { // заполнение массива рандомными числами
+        a[i] = mersenne()%10000;
     }
 
-    double start;
-    double end;
-    start = omp_get_wtime();
-        
-#pragma omp parallel for
-    for (int i = 0; i < n; i++) {
+    st = omp_get_wtime();
+
+    cout << "Выполнение алгоритма без использования прагмы:" << endl;
+    for (long i = 0; i < n; i++) {
         if (max < a[i]) {
             max = a[i];
-            cout << "Max update!" << omp_get_thread_num() << endl;
+            cout << "max = " << max << ", номер ядра, в котором исполняется код: " << omp_get_thread_num() << "\n";
         }
         if (min > a[i]) {
             min = a[i];
-            cout << "Min update!" << omp_get_thread_num() << endl;
+            cout << "min = " << min << ", номер ядра, в котором исполняется код: " << omp_get_thread_num() << "\n";
         }
     }
     end = omp_get_wtime();
-    cout << "Work took %f seconds\n"<< end - start;
-    cout << max << endl;
-    cout << min << endl;
+    cout << "Выполнение алгоритма в секундах: " << end - st << endl << "max: " << max <<" min: "<< min << endl;
+//------------------------------------------------------------------
+    cout << "Выполнение алгоритма с использованием прагмы:" << endl;
+    max = INT16_MIN;
+    min = INT16_MAX;
+    st = omp_get_wtime();
+
+    #pragma omp parallel for 
+    for (int i = 0; i < n; i++) {
+        if (max < a[i]) {
+            max = a[i];
+            cout << "max = " << max << ", номер ядра, в котором исполняется код: " << omp_get_thread_num() << "\n";
+        }
+        if (min > a[i]) {
+            min = a[i];
+            cout << "min = " << min << ", номер ядра, в котором исполняется код: " << omp_get_thread_num() << "\n";
+        }
+    }
+    end = omp_get_wtime();
+    cout << "Выполнение алгоритма в секундах: " << end - st << endl << "max: " << max << " min: " << min << endl;
 }
